75b8c9e2385c7ec8b43bfab6c21f43b1aac5029a
Yoni Slotwiner <yonislots@gmail.com>
2017-09-29 14:51:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string){
 };
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase()){
+  var lowercase = string.toLowerCase();
+  if(lowercase.toLowerCase() === lowercase){
     return "I can't hear you!";
   }
 };
